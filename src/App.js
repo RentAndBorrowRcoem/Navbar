185e87a5dae8fef67b5071a325ad45e08242eb04
Yash Ingolikar <ingolikary@gmail.com>
2023-08-24 15:18:29 +0530

    navbar pacc

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+// import Footer from './layouts/Footer';
+import Navbar from '../src/layouts/Navbar';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    // <div>
+    //   <div>
+    //     <Navbar/>
+    //   </div>
+    //   <div>
+    //     <Footer/>
+    //   </div>
+    // </div>
+    <div>
+      <Navbar/>
+    </div>
+    
+  );
+}
+
+export default App;
